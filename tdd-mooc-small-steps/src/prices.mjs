5d6da54060a2ae5c54a6418027180142870dcf6f
Tapio Salonen <tsa.dom@pm.me>
2022-01-20 03:34:44 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -72,7 +72,7 @@ function createApp(database) {
 
   function parseDate(dateString) {
     if (dateString) {
-      return new Date(dateString);
+      return Temporal.PlainDate.from(dateString);
     }
   }
 
