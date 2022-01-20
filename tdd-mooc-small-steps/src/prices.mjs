7b3a8f137615b17fd2e763159acdf481929b18b4
Tapio Salonen <tsa.dom@pm.me>
2022-01-20 03:16:43 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -61,6 +61,7 @@ function createApp(database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
+      if (!(date instanceof Date)) return true
       if (
         date &&
         date.getFullYear() === holiday.getFullYear() &&
