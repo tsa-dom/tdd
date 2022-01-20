dd2bf7d2df5c4005860851cc00dfb5f3bbed347d
Tapio Salonen <tsa.dom@pm.me>
2022-01-20 03:27:21 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -61,6 +61,7 @@ function createApp(database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = date instanceof Date ? new Date(row.holiday) : Temporal.PlainDate.from(row.holiday);
+      let tempHoliday = Temporal.PlainDate.from(row.holiday);
       if (!(date instanceof Date) && dateTemp.toString() === holiday.toString()) return true
       if (
         date.getDate() === holiday.getDate()
