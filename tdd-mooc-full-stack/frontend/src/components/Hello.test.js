6f2f6e04a4cbd71e8a0e563650e3aa9e6b7d370e
Tapio Salonen <tsa.dom@pm.me>
2022-01-24 23:33:40 +0200

    tcr: committed test files

--- tdd-mooc-full-stack/frontend/src/components/Hello.test.js
+++ tdd-mooc-full-stack/frontend/src/components/Hello.test.js
@@ -1,1 +1,16 @@
-it('', () => {})
+import React from 'react'
+import { render } from 'react-dom'
+import App from '../App'
+
+it('Renders "Hello world"', () => {
+
+  const setHelloMock = jest.fn()
+  const useStateMock = useState => [useState, setHelloMock]
+  jest.spyOn(React, 'useState').mockImplementation(useStateMock)
+
+  jest.spyOn(React, 'useEffect').mockImplementation((f) => f())
+  jest.spyOn(Hello, 'setHello')
+  render(<App />)
+
+  expect(Hello.setHello).toHaveBeenCalledTimes(1)
+})
