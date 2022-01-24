6e93cca02c315fa7a00055d47a45729718a17746
Tapio Salonen <tsa.dom@pm.me>
2022-01-24 21:18:39 +0200

    tcr: committed test files

--- tdd-mooc-full-stack/frontend/cypress/integration/tests/e2e/e2e.spec.js
+++ tdd-mooc-full-stack/frontend/cypress/integration/tests/e2e/e2e.spec.js
@@ -1,2 +1,13 @@
 /* eslint-disable no-undef */
 
+Given('An user enters to the website', () => {
+  cy.visit('http://localhost:3000')
+})
+
+When('an user clicks a button', () => {
+  cy.get('hello-button').click()
+})
+
+Then('a text "Hello world" is fetched from the database and is displayed on the webpage', () => {
+  cy.contains('Hello world')
+})
